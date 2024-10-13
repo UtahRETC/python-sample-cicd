7194c9959599b9a4549a7cf0af334e1f58f3155e
Marcos Minond <minond.marcos@gmail.com>
2024-10-13 13:58:13 -0600

    Breaking the build

--- src/app.py
+++ src/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 @app.get("/greet")
 def route_greet():
     greeting = request.args.get("greeting", "Hello")
-    name = request.args.get("name", "friend")
+    name = request.args.get("names", "friend")
     return {"message": f"{greeting} {name}"}
