676ba0e439bc49bbe01d89b96e5f37ecdb1e44f8
Marcos Minond <minond.marcos@gmail.com>
2024-10-13 14:00:32 -0600

    Fixing the tests but with bad formatting

--- src/app.py
+++ src/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.get("/greet")
 def route_greet():
-    greeting = request.args.get("greeting", "Hello")
-    name = request.args.get("names", "friend")
-    return {"message": f"{greeting} {name}"}
+  greeting = request.args.get("greeting", "Hello")
+  name = request.args.get("name", "friend")
+  return {"message": f"{greeting} {name}"}
